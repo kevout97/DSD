a22cc3b2c4b3ef2820617688855c06742dc07a6e
Mauricio Mp <mauricio.melendez@outlook.com>
2019-02-01 14:47:49 -0600

    Impares

--- Parcial1/Proyecto0/p311_5.c
+++ Parcial1/Proyecto0/p311_5.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    int num=atoi(argc);
-    printf("%d", num);
+    int num[10], suma=0;
+    for(int i=1;i<argc;i++){
+        num[i]=atoi(argv[i]);
+        suma=suma+num[i];
+    }
+    printf("La suma es: %d\n", suma);
     return 0;
 }
